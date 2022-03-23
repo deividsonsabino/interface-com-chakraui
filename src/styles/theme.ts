d0384cb4733ac962dac5f60313f99fc2de35a2ed
deividsonsabino <deividsonsabino@outlook.com.br>
2022-03-23 14:22:41 -0300

    feat: configurando paleta de cores

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,18 @@
 import { extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
+    colors: {
+        highlight: {
+            "900": "#FFBA08",
+        },
+        dark: {
+            "900": "#47585B"
+        },
+        light: {
+            "100":"#DADADA", 
+            "50": "#F5F8FA"
+        }
+    },
     fonts: {
         heading: 'Poppins',
         body: 'Poppins'
