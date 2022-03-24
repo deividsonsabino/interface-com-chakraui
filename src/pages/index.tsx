165cd48a3503581503bbb03dd06076927fc7386b
deividsonsabino <deividsonsabino@outlook.com.br>
2022-03-23 22:34:50 -0300

    feat: banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
+import { Box } from '@chakra-ui/react'
 import type { NextPage } from 'next'
+import { Banner } from '../components/Banner'
 import { Header } from '../components/Header'
 
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <Box
+      w="full"
+      mx="auto"
+      maxW="1440"
+    >
       <Header />
-    </div>
+      <Banner />
+    </Box>
   )
 }
 
